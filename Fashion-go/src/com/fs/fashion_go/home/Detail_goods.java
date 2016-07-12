0bb7b0e7729dec748a7e742f15e0210be1406785
15977270239 <2673983214@qq.com>
2016-07-12 16:39:49 +0800

    zzb

---
+++ Fashion-go/src/com/fs/fashion_go/home/Detail_goods.java
@@ -0,0 +1,28 @@
+package com.fs.fashion_go.home;
+
+import android.os.Bundle;
+
+import com.fs.fashion_go.dao.BaseActivity;
+import com.fs.fashion_go.net.MQuery;
+
+public class Detail_goods extends BaseActivity{
+	private MQuery mQuery;
+	@Override
+	public void createActivity(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initData() {
+		// TODO Auto-generated method stub
+		mQuery = new MQuery(this);
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
