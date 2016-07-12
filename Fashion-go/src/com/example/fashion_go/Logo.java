eeac06b01bbf206baaeafca1652fdebf086c61c3
15977270239 <2673983214@qq.com>
2016-07-12 10:05:08 +0800

    zzb

---
+++ Fashion-go/src/com/example/fashion_go/Logo.java
@@ -0,0 +1,14 @@
+package com.example.fashion_go;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Logo extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+}
