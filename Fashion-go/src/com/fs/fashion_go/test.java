edb72b4e26cdbc3fd67d6c63f9fa81ace588254a
Administrator <Administrator@RI9MSWQLIKMWM0N>
2016-07-12 16:09:53 +0800

    213213213213213213213213213213213213213213

--- Fashion-go/src/com/fs/fashion_go/test.java
+++ Fashion-go/src/com/fs/fashion_go/test.java
@@ -1,5 +1,23 @@
 package com.fs.fashion_go;
 
-public class test {
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
 
+public class test extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_item);
+		
+	}
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	
+	
 }
